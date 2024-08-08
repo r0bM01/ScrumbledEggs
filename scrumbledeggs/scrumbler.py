620be2927806d7ce2c66dba91cf41f3bfafb241d
r0bM01 <111903628+r0bM01@users.noreply.github.com>
2024-08-08 09:31:07 +0100

    stable

--- scrumbledeggs/scrumbler.py
+++ scrumbledeggs/scrumbler.py
@@ -3,73 +3,106 @@ import hashlib, time, secrets, random, base64
 class Crypto:
     def __init__(self):
         self.block_size = 2
-        self.hash_chunk = 3
+        self.xor_size = 3
+        self.auth_size = 4
         
-        self.pad_code = bytes.fromhex("80") * 3
+        self.pad_code = bytes.fromhex("80") * self.xor_size
         
-        self.psw_hash = False
-        self.xor_chunk = False
-        
-        self.encryption_lookup = {}
-        self.decryption_lookup = {}
-        
-        self.data = False
+        self.result_data = None
     
-    def bitwise_op(self, data):
-        tmp_data = int(data.hex(), 16)
-        xor_op = int(self.xor_chunk.hex(), 16)
-        return bytes.fromhex(hex(tmp_data ^ xor_op)[2:].zfill(6))
+    def get_hash(self, data, digest_size = 64, key = b""):
+        return hashlib.blake2b(hashlib.blake2b(data).digest(), digest_size = digest_size, key = key).digest()
     
+    def get_keyed_password(self, password):
+        return self.get_hash(data = password, key = self.get_hash(password))
+    
+    def bitwise(self, data_a, data_b):
+        zero_fill = len(data_a) * 2 
+        return bytes.fromhex( hex(int(data_a.hex(), 16) ^ int(data_b.hex(), 16))[2:].zfill(zero_fill) )
+    
+    def get_padded_data(self, data):
+        if len(data) % 2:
+            pos = random.randint(0, len(data) - 1)
+            data = data[:pos] + self.pad_code + data[pos:]
+        return data
+    
+    def get_depadded_data(self, data):
+        if self.pad_code in data:
+            pos = data.find(self.pad_code)
+            data = data[:pos] + data[pos + len(self.pad_code):]
+        return data
+    
+    def get_bytes_data(self, data):
+        if isinstance(data, bytes):
+            bytes_data = base64.b64decode(data)
+        elif isinstance(data.decode('utf-8'), str):
+            bytes_data = bytes.fromhex(data)
+        #elif isinstance(data, bytes):
+            #bytes_data = data
+        return bytes_data
+    
+    def get_decryption_table(self, keyed_password):
+        table = {}
+        for i in range(0xffff):
+            byte_num = bytes.fromhex(hex(i)[2:].zfill(4))
+            hash_value = self.get_hash(data = byte_num, digest_size = self.xor_size, key = keyed_password)
+            table[hash_value] = byte_num
+        return table
+    
+    def get_encrypted_data(self, data, password):
+        keyed_password = self.get_keyed_password(password)
+        padded_data = self.get_padded_data(data)
+        
+        bytes_group = [padded_data[x:x+self.block_size] for x in range(0, len(padded_data), self.block_size)]
+        bytes_hash = [self.get_hash(b, self.xor_size, keyed_password) for b in bytes_group]
+        bytes_encrypted = [self.bitwise(b, self.get_hash(keyed_password, self.xor_size)) for b in bytes_hash]
+        return b"".join(bytes_encrypted)
+    
+    def get_decrypted_data(self, data, password):
+        keyed_password = self.get_keyed_password(password)
+        decryption_table = self.get_decryption_table(keyed_password)
+        
+        bytes_group = [data[x:x+self.xor_size] for x in range(0, len(data), self.xor_size)]
+        bytes_hash = [self.bitwise(b, self.get_hash(keyed_password, self.xor_size)) for b in bytes_group]
+        bytes_decrypted = [decryption_table[b] for b in bytes_hash]
+        return b"".join(bytes_decrypted)
+    
+    def digest(self):
+        return self.result_data
+    
+    def hexdigest(self):
+        return self.result_data.hex()
+    
+    def b64digest(self):
+        b64 = base64.b64encode(self.result_data)
+        return b64.decode('utf-8')
+    
+    def strdigest(self):
+        return self.result_data.decode('utf-8')
+
     def sign(self, data, password):
-        psw = hashlib.blake2b(password.encode('utf-8'), digest_size = 64).digest()
-        signature = hashlib.blake2b(data, digest_size = 4, key = psw).digest()
-        return signature
+        return self.get_hash(data, digest_size = self.auth_size, key = self.get_keyed_password(password))
     
     def verify(self, data, password):
-        psw = hashlib.blake2b(password.encode('utf-8'), digest_size = 64).digest()
         signature = data[-4:]
-        tmp_data = data[:-4]
-        if hashlib.blake2b(tmp_data, digest_size = 4, key = psw).digest() == signature:
-            return tmp_data
-        else:
-            assert print("Invalid signature!!")
-    
-    def set_password(self, password):
-        psw = hashlib.blake2b(password.encode('utf-8'), digest_size = 64).digest()
-        self.xor_chunk = hashlib.blake2b(psw, digest_size = self.hash_chunk).digest()
-        for i in range(65536):
-            hex_num = bytes.fromhex(hex(i)[2:].zfill(4))
-            hash_value = hashlib.blake2b(hex_num, digest_size = self.hash_chunk, key = psw ).digest()
-            self.encryption_lookup[hex_num] = hash_value
-            self.decryption_lookup[hash_value] = hex_num
-    
-    def set_data(self, data):
-        tmp_data = data.encode('utf-8')
-        if len(tmp_data) % 2:
-            pos = random.randint(0, len(tmp_data) - 1)
-            tmp_data = tmp_data[:pos] + self.pad_code + tmp_data[pos:]
-        data_chunks = [ tmp_data[x:x+2] for x in range(0, len(tmp_data), 2) ]
-        hashed_chunks = [ self.encryption_lookup[c] for c in data_chunks ]
-        xorred_chunks = [ self.bitwise_op(c) for c in hashed_chunks ]
-        data_concat = b"".join(xorred_chunks)
-        return data_concat
+        encrypted_data = data[:-4]
+        check = self.get_hash(encrypted_data, digest_size = self.auth_size, key = self.get_keyed_password(password))
+        if check != signature:
+            raise ValueError("Password Error!")
+        return encrypted_data
         
-    def encrypt(self, data, password):
-        self.set_password(password)
-        encrypted_data = self.set_data(data)
-        authenticated_data = encrypted_data + self.sign(encrypted_data, password)
-        tmp = base64.b64encode(authenticated_data)
-        return tmp.decode()
-    
-    def decrypt(self, data, password):
-        self.set_password(password)
-        encrypted_data = base64.b64decode(data.encode())
-        encrypted_data = self.verify(encrypted_data, password)
-        xorred_chunks = [ encrypted_data[c:c+3] for c in range(0, len(encrypted_data), 3) ]
-        hashed_chunks = [ self.bitwise_op(c) for c in xorred_chunks ]
-        data_chunks = [ self.decryption_lookup[c] for c in hashed_chunks ]
-        tmp_data = b"".join(data_chunks)
-        if self.pad_code in tmp_data:
-            pos = tmp_data.find(self.pad_code)
-            tmp_data = tmp_data[:pos] + tmp_data[pos + 3:]
-        return tmp_data.decode('utf-8')
+    def encrypt(self, message, password, auth = True):
+        self.result_data = None
+        encrypted_data = self.get_encrypted_data(message.encode('utf-8'), password.encode('utf-8'))
+        if auth:
+            self.result_data = encrypted_data + self.sign(encrypted_data, password.encode('utf-8'))
+        else:
+            self.result_data = encrypted_data
+        
+    def decrypt(self, encrypted_message, password, auth = True):
+        self.result_data = None
+        data = self.get_bytes_data(encrypted_message.encode('utf-8'))
+        data = self.verify(data, password.encode('utf-8'))
+
+        decrypted_data = self.get_decrypted_data(data, password.encode('utf-8'))
+        self.result_data = self.get_depadded_data(decrypted_data)
